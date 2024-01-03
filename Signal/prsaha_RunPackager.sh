2bdbe7a3ca7e396cb4cdf7f25d90d8a0a6637478
Kuldeep Pal <kpal@cern.ch>
2024-01-03 11:21:50 +0100

    changed Signal/prsaha_* to output expanded commands without running it

--- Signal/prsaha_RunPackager.sh
+++ Signal/prsaha_RunPackager.sh
@@ -2,12 +2,12 @@
 INPUTDIR="/eos/user/p/prsaha/Tprime_analysis/FinalFit_inputs/UL/v2"
 echo $INPUTDIR
 	for mass in 600 625 650 675 700 800 900 1000 1100 1200
-	do 
+	do
 	echo "Running------------------MASS:$mass----------------------------------------"
-	echo ""
-	echo ""
+#	echo ""
+#	echo ""
 	echo "python RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts Tprime${mass}_2016,Tprime${mass}_2017,Tprime${mass}_2018 --mergeYears --printOnly --batch condor --queue espresso --massPoints 125 --outputExt packaged_Tprime${mass}"
-	python RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts Tprime${mass}_2016,Tprime${mass}_2017,Tprime${mass}_2018 --mergeYears --batch condor --queue espresso --massPoints 125 --outputExt packaged_Tprime${mass} #--printOnly
-        echo ""
-        echo ""
+#	python RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts Tprime${mass}_2016,Tprime${mass}_2017,Tprime${mass}_2018 --mergeYears --batch condor --queue espresso --massPoints 125 --outputExt packaged_Tprime${mass} #--printOnly
+#        echo ""
+#        echo ""
 	done
--- Signal/prsaha_signalFit.sh
+++ Signal/prsaha_signalFit.sh
@@ -2,16 +2,16 @@
 INPUTDIR="/eos/user/p/prsaha/Tprime_analysis/FinalFit_inputs/UL/v2"
 EXT=""
 echo $INPUTDIR
-for year in 2016 2017 2018
+for year in 2018
 do
-	for mass in 600 625 650 675 700 800 900 1000 1100 1200
-	do 
+	for mass in 700 800 900 1000 1100 1200
+	do
 	echo "Running------------------YEAR:$year MASS:$mass----------------------------------------"
-	echo ""
-	echo ""
+	#echo ""
+	#echo ""
 	echo "python RunSignalScripts.py --inputConfig config_Tprime.py --mode fTest --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --modeOpts "--nProcsToFTest -1 --doPlots""
-#	python RunSignalScripts.py --inputConfig config_Tprime.py --mode calcPhotonSyst --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --procs Tprime${mass},TTH,THQ,VBF,GG2H,VH 
-	python RunSignalScripts.py --inputConfig config_Tprime.py --mode signalFit --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --procs Tprime${mass},TTH,THQ,VBF,GG2H,VH --modeOpts "--skipVertexScenarioSplit --doPlots" #--replacementThreshold 50 #--printOnly
+#	python RunSignalScripts.py --inputConfig config_Tprime.py --mode calcPhotonSyst --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --procs Tprime${mass},TTH,THQ,VBF,GG2H,VH
+	#python RunSignalScripts.py --inputConfig config_Tprime.py --mode signalFit --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --procs Tprime${mass},TTH,THQ,VBF,GG2H,VH --modeOpts "--skipVertexScenarioSplit --doPlots" #--replacementThreshold 50 #--printOnly
 #   python RunSignalScripts.py --inputConfig config_Tprime.py --mode signalFit --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --procs Tprime${mass} --modeOpts "--skipVertexScenarioSplit --doPlots"
 #    python RunSignalScripts.py --inputConfig config_Tprime.py --mode calcPhotonSyst --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --procs Tprime${mass} #--modeOpts "--skipVertexScenarioSplit --doPlots" #--replacementThreshold 50 #--printOnly
 #    python RunSignalScripts.py --inputConfig config_Tprime.py --mode signalFit --inputWSDir $INPUTDIR/$mass/$year --ext Tprime${mass}_${year} --year $year --analysis Tprime_$mass --procs Tprime${mass} --modeOpts "--skipVertexScenarioSplit --doPlots" #--replacementThreshold 50 #--printOnly
