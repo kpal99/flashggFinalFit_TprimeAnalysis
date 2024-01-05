02007c91b47c54f9df3d5238a08a9697c734502b
Kuldeep Pal <kpal@cern.ch>
2024-01-05 11:52:11 +0100

    added runfTest_.sh to get fTest of signal module, need some more work to streamline

---
+++ Signal/runfTest_.sh
@@ -0,0 +1,1 @@
+for m in {7,10,14,20,24}00; for d in 10 30; python RunSignalScripts.py --inputConfig config_Tprime.py --mode fTest --inputWSDir ~eos/tprime_ww/test1_tprime/storeChanged/ws_TprimeM"$m"Decay"$d"pctSch  --ext TprimeM"$m"Decay"$d"pctSch --year 2018 --analysis TprimeM"$m" --procs TprimeM"$m"Decay"$d"pctSch --modeOpts "--nProcsToFTest -1 --doPlots" --printOnly
