4921a270e5cc02184fa24f209eea2620f78599ca
Kuldeep Pal <kpal@cern.ch>
2024-01-09 12:17:35 +0100

    added INPUTDIR variable in sript trees2ws_higgs_.sh

--- Trees2WS/trees2ws_higgs_.sh
+++ Trees2WS/trees2ws_higgs_.sh
@@ -10,10 +10,12 @@ case $opt in
 esac
 done
 
+INPUTDIR=/eos/user/k/kpal/tprime_ww/full1_bkg_h/storeChanged
 for i in vbf tth ggh
 do
-    echo python trees2ws.py --inputConfig config_test.py --inputTreeFile ~eos/tprime_ww/full1_bkg_h/storeChanged/output_"$i".root --productionMode "$i" --year 2018
+    echo python trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/output_"$i".root --productionMode "$i" --year 2018
     if $RUN; then
-        python trees2ws.py --inputConfig config_test.py --inputTreeFile ~eos/tprime_ww/full1_bkg_h/storeChanged/output_"$i".root --productionMode "$i" --year 2019
+        python trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/output_"$i".root --productionMode "$i" --year 2018
+        echo
     fi
 done
