68368beb5eded61935a958f3b1f3be4af5402ddc
Kuldeep Pal <kpal@cern.ch>
2024-01-16 10:16:37 +0100

    added -s flag for sync in runPlotter_*.sh

--- Signal/runPlotter_.sh
+++ Signal/runPlotter_.sh
@@ -3,38 +3,49 @@
 RUN=true
 MKDIR=false
 RUNCREATEDSCRIPTS=false
+SYNC=false
 # get the options passed to the script
-while getopts "nrh" opt;
+while getopts "nrsh" opt;
 do
 case $opt in
     m) MKDIR=true;;
     n) RUN=false;;
     r) RUNCREATEDSCRIPTS=true;;
-    h) echo "Usage: $0 [-n] [-r]"
+    s) SYNC=true;;
+    h) echo "Usage: $0 [-n] [-r] [-s]"
        echo "  -n: dry run, just print the commands to be run for any given flag"
        echo "  -r: run final Scripts which are found in outdir_*"
+       echo "  -s: cp plots to eos, with -n for dry run"
        exit 0;;
     \?) exit ;;
 esac
 done
 
+EOSDIR=/eos/user/k/kpal/www/dRjvB48p6Y/plots/finalFit
 export PYTHONPATH=$PYTHONPATH:$CMSSW_BASE/src/flashggFinalFit/tools:$CMSSW_BASE/src/flashggFinalFit/Signal/tools
 for m in {7,10,14,20,24}00
 do
     for d in 10 30
     do
-        if $RUNCREATEDSCRIPTS; then
-            for cat in THQLeptonicTag THQHadronicTag
-            do
+        for cat in THQLeptonicTag THQHadronicTag
+        do
+            if $RUNCREATEDSCRIPTS; then
                 echo python RunPlotter.py --procs all --years 2018 --cats $cat --ext packaged_TprimeM"$m"Decay"$d"pctSch
                 if $RUN; then
                     python RunPlotter.py --procs all --years 2018 --cats $cat --ext packaged_TprimeM"$m"Decay"$d"pctSch
                     echo   # to add new line after output of above script
                 fi
-            done
-        fi
+            fi
+            if $SYNC; then
+                for extension in pdf png C
+                do
+                    echo cp -v outdir_packaged_TprimeM"$m"Decay"$d"pctSch/Plots/smodel_"$cat"_2018."$extension" $EOSDIR/TprimeM"$m"Decay"$d"pctSch_smodel_"$cat"_2018."$extension"
+                    if $RUN; then
+                        cp -v outdir_packaged_TprimeM"$m"Decay"$d"pctSch/Plots/smodel_"$cat"_2018."$extension" $EOSDIR/TprimeM"$m"Decay"$d"pctSch_smodel_"$cat"_2018."$extension"
+                    fi
+                done
+                echo
+            fi
+        done
     done
 done
-
-# to sync output
-#for m in {7,10,14,20,24}00; for d in 10 30; for cat in THQLeptonicTag THQHadronicTag; for extension in pdf png C; cp -v outdir_packaged_TprimeM"$m"Decay"$d"pctSch/Plots/smodel_"$cat"_2018."$extension" ~b2g/plots/finalFit/TprimeM"$m"Decay"$d"pctSch_smodel_"$cat"_2018."$extension"
--- Signal/runPlotter_higgs_.sh
+++ Signal/runPlotter_higgs_.sh
@@ -3,31 +3,45 @@
 RUN=true
 MKDIR=false
 RUNCREATEDSCRIPTS=false
+SYNC=false
 # get the options passed to the script
-while getopts "nrh" opt;
+while getopts "nrsh" opt;
 do
 case $opt in
     n) RUN=false;;
     r) RUNCREATEDSCRIPTS=true;;
-    h) echo "Usage: $0 [-n] [-r]"
+    s) SYNC=true;;
+    h) echo "Usage: $0 [-n] [-r] [-s]"
        echo "  -n: dry run, just print the commands to be run for any given flag"
        echo "  -r: run final scripts which are found in outdir_*"
+       echo "  -s: cp plots to eos, with -n for dry run"
        exit 0;;
     \?) exit ;;
 esac
 done
 
+EOSDIR=/eos/user/k/kpal/www/dRjvB48p6Y/plots/finalFit
 export PYTHONPATH=$PYTHONPATH:$CMSSW_BASE/src/flashggFinalFit/tools:$CMSSW_BASE/src/flashggFinalFit/Signal/tools
 for i in VBF TTH #GG2H , since GG2H interal is negative
 do
-    if $RUNCREATEDSCRIPTS; then
-        for cat in THQLeptonicTag THQHadronicTag
-        do
+    for cat in THQLeptonicTag THQHadronicTag
+    do
+        if $RUNCREATEDSCRIPTS; then
             echo python RunPlotter.py --procs all --years 2018 --cats $cat --ext packaged_$i
             if $RUN; then
                 python RunPlotter.py --procs all --years 2018 --cats $cat --ext packaged_$i
                 echo   # to add new line after output of above script
             fi
-        done
-    fi
+        fi
+        if $SYNC; then
+            for extension in pdf png C
+            do
+                echo cp -v outdir_packaged_$i/Plots/smodel_"$cat"_2018."$extension" $EOSDIR/"$i"_smodel_"$cat"_2018."$extension"
+                if $RUN; then
+                    cp -v outdir_packaged_$i/Plots/smodel_"$cat"_2018."$extension" $EOSDIR/"$i"_smodel_"$cat"_2018."$extension"
+                fi
+            done
+            echo
+        fi
+    done
 done
