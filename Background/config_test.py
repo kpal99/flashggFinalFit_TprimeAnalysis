6497cbbcac92420e1a3f2240d3a2f0b3b17d7188
Kuldeep Pal <kpal@cern.ch>
2024-01-17 10:32:54 +0100

    changed Background/config_test.py according to my setup

--- Background/config_test.py
+++ Background/config_test.py
@@ -3,14 +3,15 @@
 backgroundScriptCfg = {
   
   # Setup
-  'inputWSDir':'/eos/user/p/prsaha/Tprime_analysis/FinalFit_inputs/v7/600_700/', # location of 'allData.root' file
+  'inputWSDir':'/eos/user/k/kpal/tprime_ww/full1_data2018/ws', # location of 'allData.root' file
   'cats':'THQLeptonicTag,THQHadronicTag', # auto: automatically inferred from input ws
   'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
-  'ext':'Tprime600_700', # extension to add to output directory
-  'year':'combined', # Use combined when merging all years in category (for plots)
+  'ext':'Tprime', # extension to add to output directory
+  'year':'2018', # Use combined when merging all years in category (for plots)
 
   # Job submission options
   'batch':'condor', # [condor,SGE,IC,local]
   'queue':'microcentury' # for condor e.g. microcentury
   
 }
+
