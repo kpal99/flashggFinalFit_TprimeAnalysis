478dec6c59c70cd7b4e080b63bd0e797f7c75c5c
Kuldeep Pal <kpal@cern.ch>
2024-01-18 11:59:37 +0100

    ext: bkg in Background/config_test.py

--- Background/config_test.py
+++ Background/config_test.py
@@ -6,7 +6,7 @@ backgroundScriptCfg = {
   'inputWSDir':'/eos/user/k/kpal/tprime_ww/full1_data2018/ws', # location of 'allData.root' file
   'cats':'THQLeptonicTag,THQHadronicTag', # auto: automatically inferred from input ws
   'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
-  'ext':'Tprime', # extension to add to output directory
+  'ext':'bkg', # extension to add to output directory
   'year':'2018', # Use combined when merging all years in category (for plots)
 
   # Job submission options
