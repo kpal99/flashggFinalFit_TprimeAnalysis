187659412fa13183076d43ad5ce4485eeb49a368
Kuldeep Pal <kpal@cern.ch>
2024-01-18 12:01:02 +0100

    added flags in runBackground_.sh

--- Background/runBackground_.sh
+++ Background/runBackground_.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-echo "Running 600-700 background Model"
+RUN=true
+MKDIR=false
+RUNCREATEDSCRIPTS=false
+# get the options passed to the script
+while getopts "mnrh" opt;
+do
+case $opt in
+    m) MKDIR=true;;
+    n) RUN=false;;
+    r) RUNCREATEDSCRIPTS=true;;
+    h) echo "Usage: $0 [-m] [-n] [-r]"
+       echo "  -h: print this help"
+       echo "  -m: create required directories for fTest"
+       echo "  -n: dry run, just print the commands to be run for any given flag"
+       echo "  -r: run final scripts which are found in outdir_*"
+       exit 0;;
+    \?) exit ;;
+esac
+done
 
-python RunBackgroundScripts.py --inputConfig config_600_700.py --mode fTestParallel
-
-#python RunBackgroundScripts.py --inputConfig config_HiggsCR.py --mode fTestParallel
-
-
-sleep 20s
-
-echo "Running 800-1000 background Model"
-
-python RunBackgroundScripts.py --inputConfig config_800_1000.py --mode fTestParallel
-
-sleep 20s
-
-echo "Running 1100-1200 background Model"
-
-python RunBackgroundScripts.py --inputConfig config_1100_1200.py --mode fTestParallel
+if $MKDIR; then
+    echo python RunBackgroundScripts.py --inputConfig config_test.py --mode fTestParallel --printOnly
+    if $RUN; then
+        python RunBackgroundScripts.py --inputConfig config_test.py --mode fTestParallel --printOnly
+        echo   # to add new line after output of above script
+    fi
+fi
+if $RUNCREATEDSCRIPTS; then
+    for cat in THQLeptonicTag THQHadronicTag
+    do
+        echo ./runBackgroundScripts.sh -i /eos/user/k/kpal/tprime_ww/full1_data2018/ws/allData.root -p none -f $cat --ext bkg --catOffset 0 --intLumi 59.83 --year 2018 --batch condor --queue microcentury --sigFile none --isData --fTest
+        if $RUN; then
+        ./runBackgroundScripts.sh -i /eos/user/k/kpal/tprime_ww/full1_data2018/ws/allData.root -p none -f $cat --ext bkg --catOffset 0 --intLumi 59.83 --year 2018 --batch condor --queue microcentury --sigFile none --isData --fTest
+            echo   # to add new line after output of above script
+        fi
+    done
+fi
