93953c874dd34dce4a2ddb4f032ef2fc30c27be4
Kuldeep Pal <kpal@cern.ch>
2024-06-17 22:20:43 +0200

    added -s -e flag in runPlotter_.sh, since smodel_* name are identical for all files

--- Signal/runPlotter_.sh
+++ Signal/runPlotter_.sh
@@ -3,17 +3,22 @@
 RUN=true
 MKDIR=false
 RUNCREATEDSCRIPTS=false
+SYNC=false
 # get the options passed to the script
-while getopts "nrhy:" opt;
+while getopts "nrhy:se:" opt;
 do
 case $opt in
     n) RUN=false;;
     r) RUNCREATEDSCRIPTS=true;;
     y) YEAR=$OPTARG;;
-    h) echo "Usage: $0 [-n] [-r] [-h]"
+    s) SYNC=true;;
+    e) EOSDIR=$OPTARG;;
+    h) echo "Usage: $0 [-n] [-r] [-h] [-s] [-e]"
        echo "  -n: dry run, just print the commands to be run for any given flag"
        echo "  -r: run final Scripts which are found in outdir_*"
        echo "  -y: year, can also take CSV i.e. '2016,2017,2018' "
+       echo "  -s: sync plots to ~b2g/plots, requires -e, -y"
+       echo "  -e: eos directory where plots are stored"
        echo "  -h: print this help message"
        exit 0;;
     \?) exit ;;
@@ -36,7 +41,18 @@ do
                         echo   # to add new line after output of above script
                     fi
                 fi
+                if $SYNC && [ "$EOSDIR" != "" ]; then
+                    for extension in pdf png C root
+                    do
+                        if $RUN; then
+                            cp -v outdir_packaged_TprimeM"$m"Decay"$d"pctSch/Plots/smodel_"$cat"_$YEAR."$extension" $EOSDIR/TprimeM"$m"Decay"$d"pctSch_smodel_"$cat"_$YEAR."$extension"
+                        else
+                            echo cp -v outdir_packaged_TprimeM"$m"Decay"$d"pctSch/Plots/smodel_"$cat"_$YEAR."$extension" $EOSDIR/TprimeM"$m"Decay"$d"pctSch_smodel_"$cat"_$YEAR."$extension"
+                        fi
+                    done
+                fi
             done
+            echo
         done
     done
 done
