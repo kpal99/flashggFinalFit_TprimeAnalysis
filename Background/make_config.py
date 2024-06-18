b891cc6c35d2ee9a3fc08b3c0b9144f7e0aa934e
Kuldeep Pal <kpal@cern.ch>
2024-06-18 12:45:39 +0200

    added required things for runBackground_.sh

---
+++ Background/make_config.py
@@ -0,0 +1,28 @@
+import argparse
+import jinja2
+import os
+import sys
+
+
+def make_config():
+    parser = argparse.ArgumentParser(description="Use to make config.py for signalFit",
+                                 epilog     ="python3 make_config.py --inputWS inputWS --year year")
+
+    parser.add_argument('--inputWS', type=str, required=True, help="input workspace")
+    parser.add_argument('--year', type=str, required=True, help="Year of the sample, e.g. 20UL17")
+
+# Parse the arguments
+    args = parser.parse_args(None if sys.argv[1:] else ['--help'])
+
+    # read templates/config.py
+    with open("template/config.py") as f:
+        configTemplate = f.read()
+
+    configText = jinja2.Template(configTemplate).render(inputWS=args.inputWS, year=args.year)
+    # save configText to a file
+    with open(f"config/config_{args.year}.py", "w") as f:
+        f.write(configText)
+
+
+if __name__ == '__main__':
+    make_config()
---
+++ Background/runBackground_.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+RUN=true
+MKDIR=false
+RUNCREATEDSCRIPTS=false
+# get the options passed to the script
+while getopts "mnrhy:w:" opt;
+do
+case $opt in
+    m) MKDIR=true;;
+    n) RUN=false;;
+    r) RUNCREATEDSCRIPTS=true;;
+    y) YEAR=$OPTARG;;
+    w) INPUTWS=$OPTARG;;
+    h) echo "Usage: $0 [-m] [-n] [-r] [-y YEAR] [-w INPUTDIR] [-h]"
+       echo "  -h: print this help"
+       echo "  -m: create required directories"
+       echo "  -n: dry run, just print the commands to be run for any given flag"
+       echo "  -y: year 2016,2017,2018,Combined"
+       echo "  -w: input directory"
+       echo "  -r: run final scripts which are found in outdir_*"
+       exit 0;;
+    \?) exit ;;
+esac
+done
+
+if $MKDIR; then
+    echo python3 make_config.py --inputWS $INPUTWS --year $YEAR
+    echo python3 RunBackgroundScripts.py --inputConfig config/config_$YEAR.py --mode fTestParallel --printOnly
+    if $RUN; then
+        python3 make_config.py --inputWS $INPUTWS --year $YEAR
+        python3 RunBackgroundScripts.py --inputConfig config/config_$YEAR.py --mode fTestParallel --printOnly
+        echo   # to add new line after output of above script
+    fi
+fi
+if $RUNCREATEDSCRIPTS; then
+    for cat in THQLeptonicTag THQHadronicTag
+    do
+        echo ./outdir_bkg/fTestParallel/jobs/sub_fTestParallel_bkg_$cat.sh
+        if $RUN; then
+            ./outdir_bkg/fTestParallel/jobs/sub_fTestParallel_bkg_$cat.sh
+            echo   # to add new line after output of above script
+        fi
+    done
+fi
