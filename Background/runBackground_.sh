d104990766df2bcbd66fd9eac7dff62c39644378
Kuldeep Pal <kpal@cern.ch>
2024-11-28 14:09:51 +0100

    commit added t,p flags

--- Background/runBackground_.sh
+++ Background/runBackground_.sh
@@ -3,18 +3,21 @@
 RUN=true
 TEST=false
 # get the options passed to the script
-while getopts "nhty:d:" opt;
+while getopts "nhty:d:p:" opt;
 do
 case $opt in
     n) RUN=false;;
     y) YEAR=$OPTARG;;
     d) INPUTDIR=$OPTARG;;
     t) TEST=true;;
-    h) echo "Usage: $0 [-y YEAR] [-d INPUTDIR] [-h] [-n] [-t]"
+    p) PLOTDIR=$OPTARG;;
+    h) echo "Usage: $0 [-y YEAR] [-d INPUTDIR] [-p PLOTDIR] [-h] [-n] [-t]"
        echo "  -h: print this help"
        echo "  -n: dry run, just print the commands to be run for any given flag"
        echo "  -y: year 2016,2017,2018,Combined"
        echo "  -d: input directory"
+       echo "  -t: test, run for single mass, decay width"
+       echo "  -p: plot directory to sync plots to"
        exit 0;;
     \?) exit ;;
 esac
@@ -31,6 +34,12 @@ do
             python3 make_config.py --inputWS $INPUTDIR/$TPRIMEPROC/ws/allData.root --year $YEAR --ext $TPRIMEPROC
             python3 RunBackgroundScripts.py --inputConfig config/config_"$TPRIMEPROC"_$YEAR.py --mode fTestParallel
             echo   # to add new line after output of above script
+        fi
+
+        if [ $PLOTDIR ]; then
+          mkdir -pv $PLOTDIR/bkgfTest-Data/$TPRIMEPROC
+          rsync -ah --quiet --stats outdir_$TPRIMEPROC/bkgfTest-Data/ $PLOTDIR/bkgfTest-Data/$TPRIMEPROC/
+          cp -v $PLOTDIR/bkgfTest-Data/index.php $PLOTDIR/bkgfTest-Data/$TPRIMEPROC
         fi
         [ $TEST = true ] && break
     done
