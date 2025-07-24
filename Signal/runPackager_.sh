e87894377d92c5e61685cebd0de71dcea35fbd57
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-07-24 20:07:49 +0200

    changed runPackage_, all for each mass,decay

--- Signal/runPackager_.sh
+++ Signal/runPackager_.sh
@@ -3,33 +3,26 @@
 RUN=true
 TEST=false
 # get the options passed to the script
-while getopts "nrht" opt;
+while getopts "nrhy:s:" opt;
 do
 case $opt in
     n) RUN=false;;
     t) TEST=true;;
-    h) echo "Usage: $0 [-n] [-r] [-h]"
+    y) YEAR=$OPTARG;;
+    s) TPRIMEPROC=$OPTARG;;
+    h) echo "Usage: $0 [-n] [-h] -y YEAR -s TPRIMEPROC"
        echo "  -n: dry run, just print the commands to be run for any given flag"
-       echo "  -r: run final Scripts which are found in outdir_*"
-       echo "  -t: run test scripts"
        echo "  -h: print this help message"
+       echo "  -y: year, can also take CSV i.e. '2016,2017,2018' "
+       echo "  -s: signal process to use, TPRIMEPROC"
        exit 0;;
     \?) exit ;;
 esac
 done
 
-for m in  {7..12}00 {14,16,18,20,22,24,26}00
-do
-    for d in 5 10 20 30
-    do
-        TPRIMEPROC=TprimeM"$m"Decay"$d"pct
-        YEAR=2017
-        echo python3 RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts $TPRIMEPROC --batch local --queue espresso --massPoints 125 --outputExt packaged_$TPRIMEPROC --year $YEAR
-        if $RUN; then
-            python3 RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts $TPRIMEPROC --batch local --queue espresso --massPoints 125 --outputExt packaged_$TPRIMEPROC --year $YEAR
-            echo   # to add new line after output of above script
-            fi
-        [ $TEST = true ] && break
-    done
-    [ $TEST = true ] && break
-done
+cd $(dirname $0)
+echo python3 RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts ${TPRIMEPROC} --batch local --queue espresso --massPoints 125 --outputExt packaged_${TPRIMEPROC}_${YEAR} --year $YEAR
+if $RUN; then
+    python3 RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts ${TPRIMEPROC} --batch condor --queue espresso --massPoints 125 --outputExt packaged_${TPRIMEPROC}_${YEAR} --year $YEAR
+    echo   # to add new line after output of above script
+fi
--- Signal/runPackager_all.sh
+++ Signal/runPackager_all.sh
@@ -3,32 +3,34 @@
 RUN=true
 TEST=false
 # get the options passed to the script
-while getopts "nrht" opt;
+while getopts "nrhty:" opt;
 do
 case $opt in
     n) RUN=false;;
     t) TEST=true;;
-    h) echo "Usage: $0 [-n] [-r] [-h]"
+    y) YEAR=$OPTARG;;
+    h) echo "Usage: $0 [-n] [-r] [-h] -y YEAR"
        echo "  -n: dry run, just print the commands to be run for any given flag"
        echo "  -r: run final Scripts which are found in outdir_*"
        echo "  -t: run test scripts"
+       echo "  -y: year, can also take CSV i.e. '2016,2017,2018' "
        echo "  -h: print this help message"
        exit 0;;
     \?) exit ;;
 esac
 done
 
+cd $(dirname $0)
 for m in  {7..12}00 {14,16,18,20,22,24,26}00
 do
     for d in 5 10 20 30
     do
         TPRIMEPROC=TprimeM"$m"Decay"$d"pct
-        YEAR=2017
-        echo python3 RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts $TPRIMEPROC --batch local --queue espresso --massPoints 125 --outputExt packaged_$TPRIMEPROC --year $YEAR
         if $RUN; then
-            python3 RunPackager.py --cats THQLeptonicTag,THQHadronicTag --exts $TPRIMEPROC --batch local --queue espresso --massPoints 125 --outputExt packaged_$TPRIMEPROC --year $YEAR
-            echo   # to add new line after output of above script
-            fi
+            ./runPackager_.sh -y $YEAR -s $TPRIMEPROC
+        else
+            ./runPackager_.sh -y $YEAR -s $TPRIMEPROC -n
+        fi
         [ $TEST = true ] && break
     done
     [ $TEST = true ] && break
