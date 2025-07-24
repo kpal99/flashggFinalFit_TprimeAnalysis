e94c603bb6122de8e199f723d7d52dd6adb2a471
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-07-24 23:02:38 +0200

    added runCombine_,all.sh working for each mass,decay

--- Combine/runCombine_all.sh
+++ Combine/runCombine_all.sh
@@ -3,33 +3,33 @@
 RUN=true
 TEST=false
 # get the options passed to the script
-while getopts "nht" opt;
+while getopts "nhty:" opt;
 do
 case $opt in
     n) RUN=false;;
     t) TEST=true;;
-    h) echo "Usage: $0 [-n] [-h] [-t]"
+    y) YEAR=$OPTARG;;
+    h) echo "Usage: $0 [-n] [-h] [-t] -y YEAR"
        echo "  -n: dry run, just print the commands to be run for any given flag"
        echo "  -t: run test scripts"
+       echo "  -y: year"
        echo "  -h: print this help message"
        exit 0;;
     \?) exit ;;
 esac
 done
 
-FINALFITDIR=$CMSSW_BASE/src/flashggFinalFit/
+cd $(dirname $0)
 for m in  {7..12}00 {14,16,18,20,22,24,26}00
 do
     for d in 5 10 20 30
     do
         TPRIMEPROC=TprimeM"$m"Decay"$d"pct
-        echo text2workspace.py Datacard_$TPRIMEPROC.txt -o Datacard_"$TPRIMEPROC"_mu_inclusive.root -m 125 higgsMassRange=122,128
-        echo combine --expectSignal 1 --toys -1 --saveWorkspace --freezeParameters MH --cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --method AsymptoticLimits -m 125.38 --datacard Datacard_"$TPRIMEPROC"_mu_inclusive.root --name _$TPRIMEPROC
         if $RUN; then
-            echo   # to add new line after output of above script
-            text2workspace.py Datacard_$TPRIMEPROC.txt -o Datacard_"$TPRIMEPROC"_mu_inclusive.root -m 125 higgsMassRange=122,128
-            combine --expectSignal 1 --toys -1 --saveWorkspace --freezeParameters MH --cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --method AsymptoticLimits -m 125.38 --datacard Datacard_"$TPRIMEPROC"_mu_inclusive.root --name _$TPRIMEPROC
-            fi
+            ./runCombine_.sh -y $YEAR -s $TPRIMEPROC
+        else
+            ./runCombine_.sh -y $YEAR -s $TPRIMEPROC -n
+        fi
         [ $TEST = true ] && break
     done
     [ $TEST = true ] && break
