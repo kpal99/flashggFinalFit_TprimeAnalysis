bb34592d17b26417c85a29d31edf2f5495465df5
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-07-24 12:49:35 +0200

    added trees2ws_all.sh and changed trees2ws_.sh for single mass point

--- Trees2WS/trees2ws_.sh
+++ Trees2WS/trees2ws_.sh
@@ -4,59 +4,50 @@ RUN=true
 TEST=false
 # get the options passed to the script
 # get inputDir as argument
-while getopts "nhtd:y:" opt
+while getopts "nhd:y:p:" opt
 do
 case $opt in
     n) RUN=false;;
     d) INPUTDIR=$OPTARG;;
     y) YEAR=$OPTARG;;
-    t) TEST=true;;
+    p) TPRIMEPROC=$OPTARG;;
     h) echo "trees2ws.sh [-n] [-d inputDir] [-y year] [-t] [-h]"
        echo "  -n: dry run, do not run the script"
        echo "  -d: input directory"
        echo "  -y: year"
-       echo "  -t: test, run for single mass, decay width"
+       echo "  -p: production mode"
        echo "  -h: print this message and exit"
        exit ;;
     \?) exit ;;
 esac
 done
 
-for m in  {7..12}00 {14,16,18,20,22,24,26}00
+mkdir -pv $INPUTDIR/$TPRIMEPROC/ws
+# making Tprime workspaces
+for mode in Sch; # Tch Int;
 do
-    for d in 5 10 20 30
-    do
-        TPRIMEPROC=TprimeM"$m"Decay"$d"pct
-        mkdir -pv $INPUTDIR/$TPRIMEPROC/ws
-        # making Tprime workspaces
-        for mode in Sch; # Tch Int;
-        do
-            echo python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
-            if $RUN; then
-                python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
-                ln -svf $INPUTDIR/$TPRIMEPROC/ws_$TPRIMEPROC$mode/output_M125_$TPRIMEPROC$mode.root $INPUTDIR/$TPRIMEPROC/ws
-                echo
-            fi
-        done
-
-        # making higgs workspaces
-        for higgsMode in GG2H THQ TTH VBF VH;
-        do
-            echo python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
-            if $RUN; then
-                python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
-                ln -svf $INPUTDIR/$TPRIMEPROC/ws_$higgsMode/output_M125_$higgsMode.root $INPUTDIR/$TPRIMEPROC/ws
-                echo
-            fi
-        done
-
-        # making data workspaces
-        echo python3 trees2ws_data.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
-        if $RUN; then
-            python3 trees2ws_data.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
-            echo
-        fi
-        [ $TEST = true ] && break
-    done
-    [ $TEST = true ] && break
+    echo python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
+    if $RUN; then
+        python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
+        ln -svf $INPUTDIR/$TPRIMEPROC/ws_$TPRIMEPROC$mode/output_M125_$TPRIMEPROC$mode.root $INPUTDIR/$TPRIMEPROC/ws
+        echo
+    fi
 done
+
+# making higgs workspaces
+for higgsMode in GG2H THQ TTH VBF VH;
+do
+    echo python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
+    if $RUN; then
+        python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
+        ln -svf $INPUTDIR/$TPRIMEPROC/ws_$higgsMode/output_M125_$higgsMode.root $INPUTDIR/$TPRIMEPROC/ws
+        echo
+    fi
+done
+
+# making data workspaces
+echo python3 trees2ws_data.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
+if $RUN; then
+    python3 trees2ws_data.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
+    echo
+fi
---
+++ Trees2WS/trees2ws_all.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RUN=true
+TEST=false
+# get the options passed to the script
+# get inputDir as argument
+while getopts "nhtd:y:" opt
+do
+case $opt in
+    n) RUN=false;;
+    d) INPUTDIR=$OPTARG;;
+    y) YEAR=$OPTARG;;
+    t) TEST=true;;
+    h) echo "trees2ws.sh [-n] [-d inputDir] [-y year] [-t] [-h]"
+       echo "  -n: dry run, do not run the script"
+       echo "  -d: input directory"
+       echo "  -y: year"
+       echo "  -t: test, run for single mass, decay width"
+       echo "  -h: print this message and exit"
+       exit ;;
+    \?) exit ;;
+esac
+done
+
+cd $(dirname $0)
+for m in  {7..12}00 {14,16,18,20,22,24,26}00
+do
+    for d in 5 10 20 30
+    do
+        TPRIMEPROC=TprimeM"$m"Decay"$d"pct
+        if $RUN; then
+            ./trees2ws_.sh -d $INPUTDIR -y $YEAR -p $TPRIMEPROC
+            echo
+        else
+            ./trees2ws_.sh -d $INPUTDIR -y $YEAR -p $TPRIMEPROC -n
+            echo
+        fi
+
+        [ $TEST = true ] && break
+    done
+    [ $TEST = true ] && break
+done
