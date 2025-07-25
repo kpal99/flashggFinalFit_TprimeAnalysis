f2076e0a44ec11c019a92c16e5ba1b8797ce58cd
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-07-25 08:13:42 +0200

    changed tree2ws_.sh for various different .py file

--- Trees2WS/trees2ws_.sh
+++ Trees2WS/trees2ws_.sh
@@ -26,9 +26,9 @@ mkdir -pv $INPUTDIR/$TPRIMEPROC/ws
 # making Tprime workspaces
 for mode in Sch; # Tch Int;
 do
-    echo python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
+    echo python3 trees2ws.py --inputConfig config_${YEAR}.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
     if $RUN; then
-        python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
+        python3 trees2ws.py --inputConfig config_${YEAR}.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$TPRIMEPROC$mode.root --productionMode $TPRIMEPROC$mode --year $YEAR
         ln -svf $INPUTDIR/$TPRIMEPROC/ws_$TPRIMEPROC$mode/output_M125_$TPRIMEPROC$mode.root $INPUTDIR/$TPRIMEPROC/ws
         echo
     fi
@@ -37,17 +37,17 @@ done
 # making higgs workspaces
 for higgsMode in GG2H THQ TTH VBF VH;
 do
-    echo python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
+    echo python3 trees2ws.py --inputConfig config_${YEAR}.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
     if $RUN; then
-        python3 trees2ws.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
+        python3 trees2ws.py --inputConfig config_${YEAR}.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/output_M125_$higgsMode.root --productionMode $higgsMode --year $YEAR
         ln -svf $INPUTDIR/$TPRIMEPROC/ws_$higgsMode/output_M125_$higgsMode.root $INPUTDIR/$TPRIMEPROC/ws
         echo
     fi
 done
 
 # making data workspaces
-echo python3 trees2ws_data.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
+echo python3 trees2ws_data.py --inputConfig config_${YEAR}.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
 if $RUN; then
-    python3 trees2ws_data.py --inputConfig config_test.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
+    python3 trees2ws_data.py --inputConfig config_${YEAR}.py --inputTreeFile $INPUTDIR/$TPRIMEPROC/allData.root
     echo
 fi
