84960a3625eb04ed136f407e39f9d7ff200d7701
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-07-26 09:48:48 +0200

    changed run_condorSubmitFullFlow.py flags order

--- run_condorSubmitFullFlow.py
+++ run_condorSubmitFullFlow.py
@@ -1,1 +1,1 @@
-python3 condorSubmitFullFlow.py --year 2017 --finalSelectionDir ~eos/tprime-hdna/output2.5-UL17 --plotsOutputDir ~b2g/plots/fgg-fits --tprimeMass 700,800,900,1000,1100,1200,1400,1600,1800,2000,2200,2400,2600 --tprimeDecayWidth 5,10,20,30
+python3 condorSubmitFullFlow.py --plotsOutputDir ~b2g/plots/fgg-fits --finalSelectionDir ~eos/tprime-hdna/output2.5-UL17 --year 2017 --tprimeMass 700,800,900,1000,1100,1200,1400,1600,1800,2000,2200,2400,2600 --tprimeDecayWidth 5,10,20,30
