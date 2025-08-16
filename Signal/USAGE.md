ecb27c23169fc061e20ea3ab5a8e8b7162abf847
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-08-16 09:05:28 +0200

    moved xsbrmap to TprimeRun2, TprimeRun3 in makeXSBRMap.py

--- Signal/USAGE.md
+++ Signal/USAGE.md
@@ -50,6 +50,5 @@ Usage: ./runPlotter_.sh [-n] [-y YEAR] [-h]
 ## Notes
   - Running following requires process in `tools/replacementMapy.py` and `tools/XSBRMap.py`
   - writing in `tools/replacementMapy.py` manually
-  - For `tools/XSBRMap.py` we have process,xs file as CSV, thus using that generating entries for `tools/XSBRMap.py`
+  - For `tools/XSBRMap.py` we have process,xs file as json, thus using that generating entries for `tools/XSBRMap.py`
     - See `tools/makeXSBRMap.py` and `tools/template/xsbrmap.txt`
-    - **TODO**: process,xs CSV have been changed to json, need to update `tools/makeXSBRMap.py` accordingly
--- Signal/tools/makeXSBRMap.py
+++ Signal/tools/makeXSBRMap.py
@@ -7,12 +7,18 @@ def xsbr(xsJsonFile):
     with open('template/xsbrmap.txt', 'r') as f:
         template = jinja2.Template(f.read())
 
+    if "run2" in xsJsonFile:
+        run = 2
+    elif "run3" in xsJsonFile:
+        run = 3
+    else:
+        raise Exception("Unknown xs json file, should contain 'run2' or 'run3'")
+
     with open(xsJsonFile, 'r') as f:
         xsData = json.load(f)
 
     tprimeMassList = [700, 800, 900, 1000, 1100, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600]
     decayWidthList = [5, 10, 20, 30]
-    xsVH = eval(xsData["VH"])
     for tprimeMass in tprimeMassList:
         for decayWidth in decayWidthList:
             process = f"TprimeM{tprimeMass}Decay{decayWidth}pct"
@@ -20,11 +26,11 @@ def xsbr(xsJsonFile):
             xsTch = eval(xsData[f"{process}Tch"])
             xsInt = eval(xsData[f"{process}Int"])
             print(template.render(
+                run = run,
                 process = process,
                 xsSch = xsSch,
                 xsTch = xsTch,
                 xsInt = xsInt,
-                xsVH = xsVH
                 ))
             print()
 
