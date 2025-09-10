11dff78b8d9b49ce4bb9c1ca259ad465ac8e3ab0
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-09-10 08:17:02 +0200

    corrected help in --year flag in Signal/make_config.py

--- Signal/make_config.py
+++ Signal/make_config.py
@@ -9,7 +9,7 @@ def make_config():
                                  epilog     ="python3 make_config.py --inputWSDir inputWSDir --procs procs --year year")
 
     parser.add_argument('--inputWSDir', type=str, required=True, help="input workspace directory")
-    parser.add_argument('--year', type=str, required=True, help="Year of the sample, e.g. 20UL17")
+    parser.add_argument('--year', type=str, required=True, help="Year of the sample, e.g. 2017")
     parser.add_argument('--procs', type=str, required=True, help="Process name")
 
 # Parse the arguments
