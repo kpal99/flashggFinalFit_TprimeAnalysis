43c5e8c326ecf459f0d9a57dfd4eb5c2dd2260ee
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-11-24 19:15:07 +0100

    adding zeroth implementation of runImpacts_.sh

---
+++ Combine/runImpact_.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+RUN=true
+# get the options passed to the script
+while getopts "nhy:s:p:" opt;
+do
+case $opt in
+    n) RUN=false;;
+    y) YEAR=$OPTARG;;
+    s) TPRIMEPROC=$OPTARG;;
+    p) PLOTDIR=$OPTARG;;
+    h) "Usage: $0 [-n] [-h] -y YEAR -s TPRIMEPROC"
+       "  -n: dry run, just print the commands to be run for any given flag"
+       "  -y: year"
+       "  -s: signal process to use, TPRIMEPROC"
+       "  -p: plot directory"
+       "  -h: print this help message"
+       exit 0;;
+    \?) exit ;;
+esac
+done
+
+cd $(dirname $0)
+FINALFITDIR=$CMSSW_BASE/src/flashggFinalFit/
+DATACARD=Datacard_${TPRIMEPROC}_${YEAR}_mu_inclusive.root
+
+mkdir -pv Impact/${TPRIMEPROC}_${YEAR}
+cp -v $DATACARD Impact/${TPRIMEPROC}_${YEAR}/
+cd Impact/${TPRIMEPROC}_${YEAR}
+
+COMMON_OPTS="--cminDefaultMinimizerStrategy 0 \
+--X-rtd MINIMIZER_freezeDisassociatedParams \
+--X-rtd MINIMIZER_multiMin_hideConstants \
+--X-rtd MINIMIZER_multiMin_maskConstraints \
+--X-rtd MINIMIZER_multiMin_maskChannels=2"
+
+# Run the initial fit to find the best-fit POI
+combineTool.py -M Impacts \
+    -d $DATACARD \
+    -m 125.38 \
+    --doInitialFit \
+    --robustFit 1 \
+    -t -1 \
+    --setParameters r=1 \
+    --setParameterRanges r=-0.5,3 \
+    $COMMON_OPTS \
+    --freezeParameters MH
+
+
+# Run all nuisance parameter fits locally (this may take time)
+combineTool.py -M Impacts \
+    -d $DATACARD \
+    -m 125.38 \
+    --doFits \
+    --robustFit 1 \
+    -t -1 \
+    --setParameters r=1 \
+    --setParameterRanges r=-0.5,3 \
+    $COMMON_OPTS \
+    --freezeParameters MH
+
+# Collect all fit results into one JSON file
+combineTool.py -M Impacts \
+    -d $DATACARD \
+    -m 125.38 \
+    -o impacts.json
+
+# 7. Plot the initial impacts including all parameters
+plotImpacts.py \
+    -i impacts.json \
+    -o impacts_allParams
+
+# 8. Correct impacts JSON to drop background-model parameters
+python3 ../../../Plots/correctImpacts.py \
+    --impactsJson impacts.json \
+    --dropBkgModelParams \
+    --frozenParam MH
+
+# 9. Plot the final impact plot (PDF + PNG)
+plotImpacts.py \
+    -i impacts_corrected_dropBkgModelParams.json \
+    -o impacts_corrected_dropBkgModelParams
+
+# copy to plot dir
+mkdir -pv $PLOTDIR/$TPRIMEPROC
+cp -v $PLOTDIR/index.php $PLOTDIR/$TPRIMEPROC
+cp -v impacts_allParams.pdf $PLOTDIR/$TPRIMEPROC/
+convert -density 300 impacts_allParams.pdf -quality 90 -background white -alpha remove -trim +repage impacts_allParams-%02d.png
+convert impacts_allParams-*.png -append impacts_allParams.png
+cp -v impacts_allParams.png $PLOTDIR/$TPRIMEPROC/
+
+cp -v impacts_corrected_dropBkgModelParams.pdf $PLOTDIR/$TPRIMEPROC/
+convert -density 300 impacts_corrected_dropBkgModelParams.pdf -quality 90 -background white -alpha remove -trim +repage impacts_corrected_dropBkgModelParams-%02d.png
+convert impacts_corrected_dropBkgModelParams-*.png -append impacts_corrected_dropBkgModelParams.png
+cp -v impacts_corrected_dropBkgModelParams.png $PLOTDIR/$TPRIMEPROC/
