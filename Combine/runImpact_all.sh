b464be83ab9cbb2db3dbdb957207a9a18a1c993f
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2025-11-24 19:24:18 +0100

    adding runImpact_all.sh, -n flag not implemented correctly

---
+++ Combine/runImpact_all.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+RUN=true
+TEST=false
+# get the options passed to the script
+while getopts "nhty:p:" opt;
+do
+case $opt in
+    n) RUN=false;;
+    t) TEST=true;;
+    y) YEAR=$OPTARG;;
+    p) PLOTDIR=$OPTARG;;
+    h) echo "Usage: $0 [-n] [-h] [-t] -y YEAR"
+       echo "  -n: dry run, just print the commands to be run for any given flag"
+       echo "  -t: run test scripts"
+       echo "  -y: year"
+       echo "  -p: plot directory"
+       echo "  -h: print this help message"
+       exit 0;;
+    \?) exit ;;
+esac
+done
+
+cd $(dirname $0)
+for m in  {7..12}00 {14,16,18,20,22,24,26}00
+do
+    for d in 5 10 20 30
+    do
+        TPRIMEPROC=TprimeM"$m"Decay"$d"pct
+        if $RUN; then
+            ./runImpact_.sh -y $YEAR -s $TPRIMEPROC -p $PLOTDIR
+        else
+            echo ./runImpact_.sh -y $YEAR -s $TPRIMEPROC -p $PLOTDIR -n
+        fi
+        [ $TEST = true ] && break
+    done
+    [ $TEST = true ] && break
+done
