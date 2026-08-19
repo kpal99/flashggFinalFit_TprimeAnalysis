f491cd23aea39b28ff2d1775f8d48751a13d7442
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-08-19 23:25:25 +0200

    Fix PhotonIDMVAShape typo in configs

--- Trees2WS/config_2022.py
+++ Trees2WS/config_2022.py
@@ -16,7 +16,7 @@ trees2wsCfg = {
   #'theoryWeightContainers':{'alphaSWeights':2,'scaleWeights':9,'pdfWeights':60}, # Theory weights to add to nominal + NOTAG RooDatasets, value corresponds to number of weights (0-N)
 
   # List of systematics: use string YEAR for year-dependent systematics
-  'systematics':['ElectronScale', 'ElectronSmearing', 'FNUF', 'JecSystTotal', 'JerSyst', 'Material', 'MuonResolution', 'MuonScale', 'PhotonIDMVASahpe', 'ScaleZee', 'ScaleZmmg', 'Smearing'],
+  'systematics':['ElectronScale', 'ElectronSmearing', 'FNUF', 'JecSystTotal', 'JerSyst', 'Material', 'MuonResolution', 'MuonScale', 'PhotonIDMVAShape', 'ScaleZee', 'ScaleZmmg', 'Smearing'],
   #'systematics':['FNUFEB', 'FNUFEE', 'JECAbsoluteYEAR', 'JECAbsolute', 'JECBBEC1YEAR', 'JECBBEC1', 'JECEC2YEAR', 'JECEC2', 'JECFlavorQCD', 'JECHFYEAR', 'JECHF', 'JECRelativeBal', 'JECRelativeSampleYEAR', 'JEC', 'JER', 'MCScaleGain1EB', 'MCScaleGain6EB', 'MCScaleHighR9EB', 'MCScaleHighR9EE', 'MCScaleLowR9EB', 'MCScaleLowR9EE', 'MCSmearHighR9EBPhi', 'MCSmearHighR9EBRho', 'MCSmearHighR9EEPhi', 'MCSmearHighR9EERho', 'MCSmearLowR9EBPhi', 'MCSmearLowR9EBRho', 'MCSmearLowR9EEPhi', 'MCSmearLowR9EERho', 'MaterialCentralBarrel', 'MaterialForward', 'MaterialOuterBarrel', 'MvaShift', 'PUJIDShift', 'ShowerShapeHighR9EB', 'ShowerShapeHighR9EE', 'ShowerShapeLowR9EB', 'ShowerShapeLowR9EE', 'SigmaEOverEShift', 'metJecUncertainty', 'metJerUncertainty', 'metPhoUncertainty', 'metUncUncertainty'],
 
   # Analysis categories: python list of cats or use 'auto' to extract from input tree
