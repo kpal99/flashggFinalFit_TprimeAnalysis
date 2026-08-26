41ce8a15665688ad6660cdf25c4c6f98fb21e654
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-08-26 20:12:48 +0200

    Configure multi-signal workspace mapping in runCombine_.sh

--- Combine/runCombine_.sh
+++ Combine/runCombine_.sh
@@ -24,10 +24,55 @@ done
 cd $(dirname $0)
 FINALFITDIR=$CMSSW_BASE/src/flashggFinalFit/
 
-echo text2workspace.py Datacard_${TPRIMEPROC}_${YEAR}_${systExt}.txt -o Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root -m 125 higgsMassRange=122,128
-echo combine --expectSignal 1 --toys -1 --saveWorkspace --freezeParameters MH --cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --method AsymptoticLimits -m 125.38 --datacard Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root --name _${TPRIMEPROC}_${YEAR}_${systExt}
+echo text2workspace.py Datacard_${TPRIMEPROC}_${YEAR}_${systExt}.txt \
+    --out Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root \
+    --mass 125 higgsMassRange=122,128 \
+    --physics-model HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel \
+    --physics-option "map=.*/Tprime.*:r[1,-1,1000]" \
+    --physics-option "map=.*/ggh.*:1" \
+    --physics-option "map=.*/VH.*:1" \
+    --physics-option "map=.*/tHq.*:1" \
+    --physics-option "map=.*/ttH.*:1" \
+    --physics-option "map=.*/qqH.*:1"
+
+echo combine --expectSignal 1 \
+    --toys -1 \
+    --saveWorkspace \
+    --freezeParameters MH \
+    --cminDefaultMinimizerStrategy 0 \
+    --X-rtd MINIMIZER_freezeDisassociatedParams \
+    --X-rtd MINIMIZER_multiMin_hideConstants \
+    --X-rtd MINIMIZER_multiMin_maskConstraints \
+    --X-rtd MINIMIZER_multiMin_maskChannels=2 \
+    --method AsymptoticLimits \
+    --mass 125.38 \
+    --datacard Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root \
+    --name _${TPRIMEPROC}_${YEAR}_${systExt}
+
 if $RUN; then
     echo   # to add new line after output of above script
-    text2workspace.py Datacard_${TPRIMEPROC}_${YEAR}_${systExt}.txt -o Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root -m 125 higgsMassRange=122,128
-    combine --expectSignal 1 --toys -1 --saveWorkspace --freezeParameters MH --cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --method AsymptoticLimits -m 125.38 --datacard Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root --name _${TPRIMEPROC}_${YEAR}_${systExt}
+    text2workspace.py Datacard_${TPRIMEPROC}_${YEAR}_${systExt}.txt \
+        --out Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root \
+        --mass 125 higgsMassRange=122,128 \
+        --physics-model HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel \
+        --physics-option "map=.*/Tprime.*:r[1,-1,1000]" \
+        --physics-option "map=.*/ggh.*:1" \
+        --physics-option "map=.*/VH.*:1" \
+        --physics-option "map=.*/tHq.*:1" \
+        --physics-option "map=.*/ttH.*:1" \
+        --physics-option "map=.*/qqH.*:1"
+
+    combine --expectSignal 1 \
+        --toys -1 \
+        --saveWorkspace \
+        --freezeParameters MH \
+        --cminDefaultMinimizerStrategy 0 \
+        --X-rtd MINIMIZER_freezeDisassociatedParams \
+        --X-rtd MINIMIZER_multiMin_hideConstants \
+        --X-rtd MINIMIZER_multiMin_maskConstraints \
+        --X-rtd MINIMIZER_multiMin_maskChannels=2 \
+        --method AsymptoticLimits \
+        --mass 125.38 \
+        --datacard Datacard_${TPRIMEPROC}_${YEAR}_${systExt}_mu_inclusive.root \
+        --name _${TPRIMEPROC}_${YEAR}_${systExt}
 fi
