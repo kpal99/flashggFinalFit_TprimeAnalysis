8ad50100967e1a0b6c369edbc5683d2e03aecac3
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-08-26 20:13:50 +0200

    Expand signal keywords and disable bkg fallback in makeYields

--- Datacard/makeYields.py
+++ Datacard/makeYields.py
@@ -141,10 +141,11 @@ for year in years:
 
     # Add signal process to dataFrame:
     print(" --> Adding to dataFrame: (proc,cat) = (%s,%s)"%(_proc,_cat))
-    if "tprime" in proc.lower():
+    keywords = ["tprime", "qqh", "ggh", "vh", "thq", "tth"]
+    if any(kw in proc.lower() for kw in keywords):
         data.loc[len(data)] = [year,'sig',_procOriginal,_proc,_proc_s0,_cat,_inputWSFile,_nominalDataName,_modelWSFile,_model,_rate]
-    else:
-        data.loc[len(data)] = [year,'bkg',_procOriginal,_proc,_proc_s0,_cat,_inputWSFile,_nominalDataName,_modelWSFile,_model,_rate]
+    #else:
+    #    data.loc[len(data)] = [year,'bkg',_procOriginal,_proc,_proc_s0,_cat,_inputWSFile,_nominalDataName,_modelWSFile,_model,_rate]
 
 # Background and data processes
 if( not opt.skipBkg)&( opt.cat != "NOTAG" ):
