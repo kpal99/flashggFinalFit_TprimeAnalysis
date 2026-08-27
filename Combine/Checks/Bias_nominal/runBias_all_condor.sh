682086de57c1b3d9cfda6d8bb006d8b25afd9c5b
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-08-27 21:51:11 +0200

    Add runBias_all_condor.sh

---
+++ Combine/Checks/Bias_nominal/runBias_all_condor.sh
@@ -0,0 +1,17 @@
+for M in {7,8,9,10,11,12}00 {14,16,18,20,22,24,26}00;
+do
+    for d in 5 10 20 30
+    do
+        TPRIMEPROC=TprimeM"$M"Decay"$d"pct
+        condor_wrap.py --jobFlavour microcentury --cmsenv -- ./runBias_.sh -y Run2 -s $TPRIMEPROC
+    done
+done
+
+for M in {7,8,9,10,11,12}00 {14,16,18,20,22,24,26}00;
+do
+    for d in 5 10 20 30
+    do
+        TPRIMEPROC=TprimeM"$M"Decay"$d"pct
+        condor_wrap.py --jobFlavour microcentury --cmsenv -- ./runBias_.sh -y 22plus23 -s $TPRIMEPROC
+    done
+done
