b2448770413cea0a9467437e7fcc2484d3f3b005
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-08-27 19:08:13 +0200

    Add year and signal process options to runBias_.sh

--- Combine/Checks/Bias_nominal/runBias_.sh
+++ Combine/Checks/Bias_nominal/runBias_.sh
@@ -1,25 +1,43 @@
+#!/bin/bash
+
+while getopts "hy:s:" opt;
+do
+case $opt in
+    y) YEAR=$OPTARG;;
+    s) TPRIMEPROC=$OPTARG;;
+    h) echo "Usage: $0 [-h] -y YEAR -s TPRIMEPROC"
+       echo "  -y: year"
+       echo "  -s: signal process to use, TPRIMEPROC"
+       echo "  -h: print this help message"
+       exit 0;;
+    \?) exit ;;
+esac
+done
+
+cd $(dirname "$0")
+
 echo "==> Running combineCards..."
 combineCards.py \
-    ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst.txt \
+    ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst.txt \
     --include-channel Hadronic \
-    > ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst_Hadronic.txt
+    > ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Hadronic.txt
 combineCards.py \
-    ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst.txt \
+    ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst.txt \
     --include-channel Leptonic \
-    > ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst_Leptonic.txt
+    > ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Leptonic.txt
 
 echo -e "\n==> Cleaning datacard with sed..."
-sed -i '/pdfindex_Leptonic/d' ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst_Hadronic.txt
-sed -i '/pdfindex_Hadronic/d' ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst_Leptonic.txt
+sed -i '/pdfindex_Leptonic/d' ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Hadronic.txt
+sed -i '/pdfindex_Hadronic/d' ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Leptonic.txt
 
 echo -e "\n==> Creating directory..."
-mkdir -pv Datacard/TprimeM1800Decay5pct_Run2_Hadronic/
-mkdir -pv Datacard/TprimeM1800Decay5pct_Run2_Leptonic/
+mkdir -pv Datacard/"$TPRIMEPROC"_"$YEAR"_Hadronic/
+mkdir -pv Datacard/"$TPRIMEPROC"_"$YEAR"_Leptonic/
 
 echo -e "\n==> Converting text datacard to workspace..."
 text2workspace.py \
-    ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst_Hadronic.txt \
-    --out Datacard/TprimeM1800Decay5pct_Run2_Hadronic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Hadronic.root \
+    ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Hadronic.txt \
+    --out Datacard/"$TPRIMEPROC"_"$YEAR"_Hadronic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Hadronic.root \
     --mass 125 \
     higgsMassRange=122,128 \
     --physics-model HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel \
@@ -31,8 +49,8 @@ text2workspace.py \
     --physics-option map=".*/qqH.*:1"
 
 text2workspace.py \
-    ../../Datacard_TprimeM1800Decay5pct_Run2_withSyst_Leptonic.txt \
-    --out Datacard/TprimeM1800Decay5pct_Run2_Leptonic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Leptonic.root \
+    ../../Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Leptonic.txt \
+    --out Datacard/"$TPRIMEPROC"_"$YEAR"_Leptonic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Leptonic.root \
     --mass 125 \
     higgsMassRange=122,128 \
     --physics-model HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel \
@@ -43,52 +61,55 @@ text2workspace.py \
     --physics-option map=".*/ttH.*:1" \
     --physics-option map=".*/qqH.*:1"
 
+for mu in 1
+do
 echo -e "\n==> Running bias study (toys)..."
 python3 RunBiasStudy.py \
     --split 1000 \
-    --datacard Datacard/TprimeM1800Decay5pct_Run2_Hadronic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Hadronic.root \
+    --datacard Datacard/"$TPRIMEPROC"_"$YEAR"_Hadronic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Hadronic.root \
     --toys \
     --expectSignal 1 \
-    --subDir TprimeM1800Decay5pctRun2_Hadronic_mu1
+    --subDir "$TPRIMEPROC""$YEAR"_Hadronic_mu"$mu"
 python3 RunBiasStudy.py \
     --split 1000 \
-    --datacard Datacard/TprimeM1800Decay5pct_Run2_Leptonic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Leptonic.root \
+    --datacard Datacard/"$TPRIMEPROC"_"$YEAR"_Leptonic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Leptonic.root \
     --toys \
     --expectSignal 1 \
-    --subDir TprimeM1800Decay5pctRun2_Leptonic_mu1
+    --subDir "$TPRIMEPROC""$YEAR"_Leptonic_mu"$mu"
 
 echo -e "\n==> Running bias study (fits)..."
 python3 RunBiasStudy.py \
     --split 1000 \
-    --datacard Datacard/TprimeM1800Decay5pct_Run2_Hadronic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Hadronic.root \
+    --datacard Datacard/"$TPRIMEPROC"_"$YEAR"_Hadronic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Hadronic.root \
     --fits \
     --expectSignal 1 \
-    --subDir TprimeM1800Decay5pct_Run2_Hadronic_mu1 \
+    --subDir "$TPRIMEPROC"_"$YEAR"_Hadronic_mu"$mu" \
     --combineOptions "--cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --freezeParameters MH"
 
 python3 RunBiasStudy.py \
     --split 1000 \
-    --datacard Datacard/TprimeM1800Decay5pct_Run2_Leptonic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Leptonic.root \
+    --datacard Datacard/"$TPRIMEPROC"_"$YEAR"_Leptonic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Leptonic.root \
     --fits \
     --expectSignal 1 \
-    --subDir TprimeM1800Decay5pct_Run2_Leptonic_mu1 \
+    --subDir "$TPRIMEPROC"_"$YEAR"_Leptonic_mu"$mu" \
     --combineOptions "--cminDefaultMinimizerStrategy 0 --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --freezeParameters MH"
 
 echo -e "\n==> Running bias study (plots)..."
 python3 RunBiasStudy.py \
     --split 1000 \
-    --datacard Datacard/TprimeM1800Decay5pct_Run2_Hadronic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Hadronic.root \
+    --datacard Datacard/"$TPRIMEPROC"_"$YEAR"_Hadronic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Hadronic.root \
     --plots \
     --gaussianFit \
     --expectSignal 1 \
-    --subDir TprimeM1800Decay5pct_Run2_Hadronic_mu1 \
-    2>&1 | tee BiasPlots/output_TprimeM1800Decay5pct_Run2_Hadronic_mu1.log
+    --subDir "$TPRIMEPROC"_"$YEAR"_Hadronic_mu"$mu" \
+    2>&1 | tee BiasPlots/output_"$TPRIMEPROC"_"$YEAR"_Hadronic_mu"$mu".log
 
 python3 RunBiasStudy.py \
     --split 1000 \
-    --datacard Datacard/TprimeM1800Decay5pct_Run2_Leptonic/Datacard_TprimeM1800Decay5pct_Run2_withSyst_Leptonic.root \
+    --datacard Datacard/"$TPRIMEPROC"_"$YEAR"_Leptonic/Datacard_"$TPRIMEPROC"_"$YEAR"_withSyst_Leptonic.root \
     --plots \
     --gaussianFit \
     --expectSignal 1 \
-    --subDir TprimeM1800Decay5pct_Run2_Leptonic_mu1 \
-    2>&1 | tee BiasPlots/output_TprimeM1800Decay5pct_Run2_Leptonic_mu1.log
+    --subDir "$TPRIMEPROC"_"$YEAR"_Leptonic_mu"$mu" \
+    2>&1 | tee BiasPlots/output_"$TPRIMEPROC"_"$YEAR"_Leptonic_mu"$mu".log
+done
