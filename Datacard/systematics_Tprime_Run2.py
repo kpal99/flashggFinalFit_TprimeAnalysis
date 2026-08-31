d53ed42faf52ad67bad0ed38a31b63f7c43af099
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-08-31 02:54:09 +0200

    Update theory, experimental and shape systematics lists in systematics_Tprime files

--- Datacard/systematics_Tprime_Run2.py
+++ Datacard/systematics_Tprime_Run2.py
@@ -21,53 +21,61 @@
 
 theory_systematics = [
                 # Normalisation uncertainties: enter interpretations
+                # Interim 13.6 TeV xsec theory uncertainties (per process for scale; single NP for PDF/alphaS)
+                # Values are in theory_uncs_ad_interim_13p6TeV.json and mapped to ggh/vbf/vh/bbh in/out processes
                 {'name':'BR_hgg','title':'BR_hgg','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':"0.98/1.021"},
                 {'name':'QCDscale_ggH','title':'QCDscale_ggH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_ggh.json'},
+                {'name':'QCDscale_VH','title':'QCDscale_VH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_vh.json'},
                 {'name':'QCDscale_qqH','title':'QCDscale_qqH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_qqh.json'},
-                {'name':'QCDscale_VH','title':'QCDscale_VH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_vh.json'}, # Note: VH had components accounted for in THU_qqH_*, set to 1 in json
-                #{'name':'QCDscale_ggZH','title':'QCDscale_ggZH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_ggzh.json'}, # Note: ggZH lep components are accounted for in THU_ggZH i.e. this only covers the ggZH had component
                 {'name':'QCDscale_ttH','title':'QCDscale_ttH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_tth.json'},
                 {'name':'QCDscale_tHq','title':'QCDscale_tHq','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_thq.json'},
-                #{'name':'QCDscale_tHW','title':'QCDscale_tHW','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_thw.json'},
-                #{'name':'QCDscale_bbH','title':'QCDscale_bbH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_bbh.json'},
-                {'name':'pdf_Higgs_ggH','title':'pdf_Higgs_ggH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_ggh.json'},
-                {'name':'pdf_Higgs_qqH','title':'pdf_Higgs_qqH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_qqh.json'},
-                {'name':'pdf_Higgs_VH','title':'pdf_Higgs_VH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_vh.json'},
-                #{'name':'pdf_Higgs_ggZH','title':'pdf_Higgs_ggZH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_ggzh.json'},
-                {'name':'pdf_Higgs_ttH','title':'pdf_Higgs_ttH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_tth.json'},
-                {'name':'pdf_Higgs_tHq','title':'pdf_Higgs_tHq','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_thq.json'},
-                #{'name':'pdf_Higgs_tHW','title':'pdf_Higgs_tHW','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_thw.json'},
-                {'name':'alphaS_ggH','title':'alphaS_ggH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_ggh.json'},
-                {'name':'alphaS_qqH','title':'alphaS_qqH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_qqh.json'},
-                {'name':'alphaS_VH','title':'alphaS_VH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_vh.json'},
-                #{'name':'alphaS_ggZH','title':'alphaS_ggZH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_ggzh.json'},
-                {'name':'alphaS_ttH','title':'alphaS_ttH','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_tth.json'},
-                {'name':'alphaS_tHq','title':'alphaS_tHq','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_thq.json'},
-                #{'name':'alphaS_tHW','title':'alphaS_tHW','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/thu_thw.json'},
-
-                # Shape uncertainties: enter direct XS measurements
-                # Shape uncertainties: enter direct XS measurements
-                # Scale weights grouping is defined in makeDatacard.py
-                # For some reason, the name is saved only as `Scal`, not `Scale`, do not ask me why
-                # Comment out the nominal weight here as it does not contain any `tiers`, so it would fail in `makeDatacard.py``
-                # The scheme below is valid for v13, you need to explicitly check the nanoAOD documentation to validate your setup
-                #{'name':'weight_LHEScal_0','title':'CMS_hgg_scaleWeight_0','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_LHEScal_1','title':'CMS_hgg_scaleWeight_1','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_LHEScal_2','title':'CMS_hgg_scaleWeight_2','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']}, #Unphysical
-                #{'name':'weight_LHEScal_3','title':'CMS_hgg_scaleWeight_3','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_LHEScal_4','title':'CMS_hgg_scaleWeight_4','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']}, # nominal weight
-                #{'name':'weight_LHEScal_5','title':'CMS_hgg_scaleWeight_5','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_LHEScal_6','title':'CMS_hgg_scaleWeight_6','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']}, #Unphysical
-                #{'name':'weight_LHEScal_7','title':'CMS_hgg_scaleWeight_7','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_LHEScal_8','title':'CMS_hgg_scaleWeight_8','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_AlphaS','title':'CMS_hgg_AlphaS','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_PS_ISR','title':'CMS_hgg_PS_ISR','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
-                #{'name':'weight_PS_FSR','title':'CMS_hgg_PS_FSR','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']}
-
+                # PDF and alphaS treated as single correlated NPs across processes (xsec-only)
+                #{'name':'pdf_xsec','title':'pdf_xsec','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/theory_uncs_ad_interim_13p6TeV.json'},
+                #{'name':'alphaS_xsec','title':'alphaS_xsec','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'theory_uncertainties/theory_uncs_ad_interim_13p6TeV.json'},
               ]
 
-# PDF weight
-#for i in range(1,101): theory_systematics.append( {'name':'weight_LHEPd_%g'%i, 'title':'CMS_hgg_pdfWeight_%g'%i, 'type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']} )
+# Theory weights in the nominal RooDataSets (HiggsDNA naming)
+# LHE scale weights: use the conventional 6-point subset (skip 2,4,6)
+# Decorrelate between production modes (including signal processes)
+#lhescale_procs = [
+#  ("ggH", "ggh"),
+#  ("VBF", "vbf"),
+#  ("VH",  "vh"),
+#  ("bbH", "bbh"),
+#  ("ttH", "tth"),
+#  ("tHq", "tHq"),
+#  ("tHW", "tHW"),
+#]
+#for i in (0, 1, 3, 5, 7, 8):
+#  for tag, match in lhescale_procs:
+#    theory_systematics.append({
+#      'name': f'weight_LHEScal_{tag}_{i}',
+#      'title': f'CMS_hgg_scaleWeight_{i}_{tag}',
+#      'weight_name': f'weight_LHEScal_{i}',
+#      'proc_match': match,
+#      'type': 'factory',
+#      'prior': 'lnN',
+#      'correlateAcrossYears': 1,
+#      'tiers': ['shape']
+#    })
+#
+# LHE PDF weights (1..100). Stored as weight_LHEPd_* in RooDataSets.
+#for i in range(1, 101):
+#  theory_systematics.append({
+#    'name': f'weight_LHEPd_{i}',
+#    'title': f'CMS_hgg_pdfWeight_{i}',
+#    'type': 'factory',
+#    'prior': 'lnN',
+#    'correlateAcrossYears': 1,
+#    'tiers': ['shape']
+#  })
+
+# alphaS and PS weights
+theory_systematics.extend([
+  {'name':'weight_AlphaS','title':'CMS_hgg_AlphaS','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
+  {'name':'weight_PS_ISR','title':'CMS_hgg_PS_ISR','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
+  {'name':'weight_PS_FSR','title':'CMS_hgg_PS_FSR','type':'factory','prior':'lnN','correlateAcrossYears':1,'tiers':['shape']},
+])
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -77,29 +85,60 @@ theory_systematics = [
 # correlateAcrossYears = -1 : partially correlated
 
 experimental_systematics = [
-                #{'name':'lumi_13p6TeV_2022','title':'lumi_13p6TeV_2022','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':"1.014"},
-                #{'name':'lumi_13TeV_2017','title':'lumi_13TeV_2017','type':'constant','prior':'lnN','correlateAcrossYears':1,'value':'1.023'},
+                # Run-3 correlated luminosity scheme (reduction method)
                 {'name':'lumi_13TeV_Correlated','title':'lumi_13TeV_Correlated','type':'constant','prior':'lnN','correlateAcrossYears':0,'value':{'2016':'1.012','2017':'1.023','2018':'1.025'}},
-                #{'name':'weight_Pileup','title':'CMS_hgg_PileupWeight','type':'factory','prior':'lnN','correlateAcrossYears':1},
-                #{'name':'weight_TriggerSF','title':'CMS_hgg_TriggerWeight','type':'factory','prior':'lnN','correlateAcrossYears':1},
-                #{'name':'weight_ElectronVetoSF','title':'CMS_hgg_ElectronVetoSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
-                #{'name':'weight_PreselSF','title':'CMS_hgg_PreselSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
-                #{'name':'weight_SF_photon_ID','title':'CMS_hgg_phoIdMva','type':'factory','prior':'lnN','correlateAcrossYears':1},
-                #{'name':'energyErrShift','title':'CMS_hgg_SigmaEOverEShift','type':'factory','prior':'lnN','correlateAcrossYears':1}
               ]
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Tree-based experimental systematics (Up/Down trees in Trees2WS config)
+experimental_systematics.extend([
+  #{'name':'ElectronScale','title':'CMS_hgg_ElectronScale','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'ElectronSmearing','title':'CMS_hgg_ElectronSmearing','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'JecSystTotal','title':'CMS_scale_j','type':'factory','prior':'lnN','correlateAcrossYears':0},
+  {'name':'JerSyst','title':'CMS_res_j','type':'factory','prior':'lnN','correlateAcrossYears':0},
+  {'name':'MET','title':'CMS_hgg_MET','type':'factory','prior':'lnN','correlateAcrossYears':0},
+  #{'name':'MuonResolution','title':'CMS_hgg_MuonResolution','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  #{'name':'MuonScale','title':'CMS_hgg_MuonScale','type':'factory','prior':'lnN','correlateAcrossYears':1},
+])
+
+# Weight-based experimental systematics (Up/Down weights in nominal RooDataSets)
+experimental_systematics.extend([
+  {'name':'weight_Pileup','title':'CMS_hgg_PileupWeight','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_TriggerSF','title':'CMS_hgg_TriggerWeight','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_ElectronVetoSF','title':'CMS_hgg_ElectronVetoSF','type':'factory','prior':'lnN','correlateAcrossYears':0}, # from Zmumug, main unc is statistical
+  {'name':'weight_PreselSF','title':'CMS_hgg_PreselSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_LoosePhoIDSF','title':'CMS_hgg_LoosePhoIDSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_L1Prefiring','title':'CMS_hgg_L1Prefiring','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  #{'name':'weight_NUM_MediumID_DEN_TrackerMuons','title':'CMS_hgg_NUM_MediumID_DEN_TrackerMuons','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  #{'name':'weight_NUM_TightPFIso_DEN_MediumID','title':'CMS_hgg_TightPFIso_DEN_MediumID','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_atLeast1LeptonIdSF_ele_Reco','title':'CMS_hgg_EleRecoSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_atLeast1LeptonIdSF_ele_wp90iso','title':'CMS_hgg_EleWP90IsoSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_atLeast1LeptonIdSF_mu_NUM_MediumID_DEN_TrackerMuons','title':'CMS_hgg_MuMediumIDSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_atLeast1LeptonIdSF_mu_NUM_TightPFIso_DEN_MediumID','title':'CMS_hgg_MuTightPFIsoSF','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_bTagSF_sys_cferr1','title':'CMS_hgg_bTagSF_sys_cferr1','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_bTagSF_sys_cferr2','title':'CMS_hgg_bTagSF_sys_cferr2','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_bTagSF_sys_hf','title':'CMS_hgg_bTagSF_sys_hf','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_bTagSF_sys_hfstats1','title':'CMS_hgg_bTagSF_sys_hfstats1','type':'factory','prior':'lnN','correlateAcrossYears':0},
+  {'name':'weight_bTagSF_sys_hfstats2','title':'CMS_hgg_bTagSF_sys_hfstats2','type':'factory','prior':'lnN','correlateAcrossYears':0},
+  {'name':'weight_bTagSF_sys_jes','title':'CMS_hgg_bTagSF_sys_jes','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_bTagSF_sys_lf','title':'CMS_hgg_bTagSF_sys_lf','type':'factory','prior':'lnN','correlateAcrossYears':1},
+  {'name':'weight_bTagSF_sys_lfstats1','title':'CMS_hgg_bTagSF_sys_lfstats1','type':'factory','prior':'lnN','correlateAcrossYears':0},
+  {'name':'weight_bTagSF_sys_lfstats2','title':'CMS_hgg_bTagSF_sys_lfstats2','type':'factory','prior':'lnN','correlateAcrossYears':0},
+  # ggH/VBF-specific Higgs+heavy-flavor variations (separate NPs per production mode)
+#  {'name':'weight_Higgs_plus_b_syst_ggH','weight_name':'weight_Higgs_plus_b_syst','proc_match':'ggh','title':'CMS_hgg_Higgs_plus_b_syst_ggH','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_Higgs_plus_c_syst_ggH','weight_name':'weight_Higgs_plus_c_syst','proc_match':'ggh','title':'CMS_hgg_Higgs_plus_c_syst_ggH','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_Higgs_plus_b_syst_vbf','weight_name':'weight_Higgs_plus_b_syst','proc_match':'vbf','title':'CMS_hgg_Higgs_plus_b_syst_vbf','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_Higgs_plus_c_syst_vbf','weight_name':'weight_Higgs_plus_c_syst','proc_match':'vbf','title':'CMS_hgg_Higgs_plus_c_syst_vbf','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_Higgs_plus_b_syst_VH','weight_name':'weight_Higgs_plus_b_syst','proc_match':'vh','title':'CMS_hgg_Higgs_plus_b_syst_vh','type':'factory','prior':'lnN','correlateAcrossYears':1},
+#  {'name':'weight_Higgs_plus_c_syst_VH','weight_name':'weight_Higgs_plus_c_syst','proc_match':'vh','title':'CMS_hgg_Higgs_plus_c_syst_vh','type':'factory','prior':'lnN','correlateAcrossYears':1},
+])
 
 # Shape nuisances: effect encoded in signal model
-# mode = (other,scalesGlobal,scales,scalesCorr,smears): match the definition in the signal models
-
+# mode = (other,scalesGlobal,scales,scalesCorr,smears,smearsCorr): match the definition in the signal models
 signal_shape_systematics = [
+                #{'name':'ScaleZee','title':'ScaleZee','type':'signal_shape','mode':'scalesCorr','mean':'0.0','sigma':'2.0'},
+                #{'name':'ScaleZmmg','title':'ScaleZmmg','type':'signal_shape','mode':'scalesCorr','mean':'0.0','sigma':'2.0'},
+                {'name':'ShowerShape','title':'ShowerShape','type':'signal_shape','mode':'scalesCorr','mean':'0.0','sigma':'2.0'},
+                {'name':'Smearing','title':'Smearing','type':'signal_shape','mode':'smears','mean':'0.0','sigma':'1.0'},
                 {'name':'Material','title':'Material','type':'signal_shape','mode':'scalesCorr','mean':'0.0','sigma':'1.0'},
-                {'name':'FNUF','title':'FNUF','type':'signal_shape','mode':'scalesCorr','mean':'0.0','sigma':'1.0'},
-                {'name':'ShowerShape','title':'ShowerShape','type':'signal_shape','mode':'scalesCorr','mean':'0.0','sigma':'1.0'},
-                #{'name':'deltafracright','title':'deltafracright','type':'signal_shape','mode':'other','mean':'0.0','sigma':'0.02'},
-                #{'name':'Scale','title':'Scale','type':'signal_shape','mode':'scales','mean':'0.0','sigma':'1.0'},
-                #{'name':'Smearing','title':'Smearing','type':'signal_shape','mode':'smears','mean':'0.0','sigma':'1.0'},
-                #{'name':'NonLinearity','title':'NonLinearity','type':'signal_shape','mode':'scalesGlobal','mean':'0.0','sigma':'0.002'},
-                #{'name':'Geant4','title':'Geant4','type':'signal_shape','mode':'scalesGlobal','mean':'0.0','sigma':'0.0005'}
-              ]
+                {'name':'FNUF','title':'FNUF','type':'signal_shape','mode':'scalesCorr','mean':'0.0','sigma':'1.0'}
+]
