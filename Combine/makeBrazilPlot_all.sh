f98f55b976f003bc1434c334df8f31c853c8a03b
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-09-01 10:53:24 +0200

    Remove --woSyst from makeBrazilPlot_all.sh

--- Combine/makeBrazilPlot_all.sh
+++ Combine/makeBrazilPlot_all.sh
@@ -1,14 +1,14 @@
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 5 --compare --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 10 --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 20 --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 30 --woSyst
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 5 --compare
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 10
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 20
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 30
 
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 5  --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 10 --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 20 --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 30 --woSyst
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 5
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 10
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 20
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 30
 
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 5  --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 10 --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 20 --woSyst
-python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 30 --woSyst
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 5
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 10
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 20
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 30
