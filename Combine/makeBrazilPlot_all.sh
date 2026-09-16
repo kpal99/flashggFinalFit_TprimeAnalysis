c2e009b6b08389357b4e125e5ae9fcb64243b352
Kuldeep Kumar Pal <kuldeep.pal@cern.ch>
2026-09-16 14:21:47 +0200

    Add combined decayWidth Brazil plots to makeBrazilPlot_all.sh

--- Combine/makeBrazilPlot_all.sh
+++ Combine/makeBrazilPlot_all.sh
@@ -2,13 +2,16 @@ python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 5 
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 10
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 20
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 30
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year Run2 --decayWidth 5,10,20,30 --compare
 
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 5
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 10
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 20
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 30
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year 22plus23 --decayWidth 5,10,20,30
 
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 5
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 10
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 20
 python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 30
+python3 makeBrazilPlot.py --outDir ~b2g/plots/limits --year full --decayWidth 5,10,20,30
